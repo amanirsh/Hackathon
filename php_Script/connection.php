44a1ddc19b27568a2c952929d8d9dfb03d4aa70d
anmak <anmahmad@live.com>
2015-05-18 12:04:22 -0700

    anam_ibrar_hassan

---
+++ php_Script/connection.php
@@ -0,0 +1,36 @@
+<?php 
+
+$json="";
+$host="localhost"; //replace with database hostname  
+
+$username="root"; //replace with database username  
+
+$password="root"; //replace with database password  
+
+$db_name="android_api"; //replace with database name   
+
+
+$conn = new mysqli($host, $username, $password, $db_name);
+
+// Check connection
+ if ($conn->connect_error) {
+     die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "select * from doctors";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+       $json['doctor'][]=$row;
+    }
+} else {
+    
+}
+
+
+$conn->close();
+echo json_encode($json);  
+?>
